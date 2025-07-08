e7ab8cadd2d94067ca92770fc2b43f46175c3434
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-08 16:41:34 +0200

    Refactor StoreException class to use final fields and remove unnecessary setters

--- repo_git/onlinebookstore/src/main/java/com/bittercode/model/StoreException.java
+++ repo_git/onlinebookstore/src/main/java/com/bittercode/model/StoreException.java
@@ -6,14 +6,14 @@ import com.bittercode.constant.ResponseCode;
 
 public class StoreException extends IOException {
 
-    private String errorCode;
-    private String errorMessage;
-    private int statusCode;
+    private final String errorCode;
+    private final String errorMessage;
+    private final int statusCode;
 
     public StoreException(String errorMessage) {
         super(errorMessage);
         this.errorCode = "BAD_REQUEST";
-        this.setStatusCode(400);
+        this.statusCode = 400;
         this.errorMessage = errorMessage;
     }
 
@@ -21,7 +21,7 @@ public class StoreException extends IOException {
         super(errorCodes.getMessage());
         this.statusCode = errorCodes.getCode();
         this.errorMessage = errorCodes.getMessage();
-        this.setErrorCode(errorCodes.name());
+        this.errorCode = errorCodes.name();
     }
 
     public StoreException(String errroCode, String errorMessage) {
@@ -42,24 +42,11 @@ public class StoreException extends IOException {
         return errorCode;
     }
 
-    public void setErrorCode(String errorCode) {
-        this.errorCode = errorCode;
-    }
-
     public String getErrorMessage() {
         return errorMessage;
-    }
-
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
     }
 
     public int getStatusCode() {
         return statusCode;
     }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
 }
