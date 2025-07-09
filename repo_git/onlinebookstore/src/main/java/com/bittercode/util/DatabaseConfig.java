5312e28871a28d5962f52ca5918ab6830f4276a1
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-09 14:44:23 +0200

    Refactor DatabaseConfig to use consistent static final modifier for constants

--- repo_git/onlinebookstore/src/main/java/com/bittercode/util/DatabaseConfig.java
+++ repo_git/onlinebookstore/src/main/java/com/bittercode/util/DatabaseConfig.java
@@ -19,12 +19,11 @@ class DatabaseConfig {
         }
     }
 
-    public final static String DRIVER_NAME = prop.getProperty("db.driver");
-    public final static String DB_HOST = prop.getProperty("db.host");
-    public final static String DB_PORT = prop.getProperty("db.port");
-    public final static String DB_NAME = prop.getProperty("db.name");
-    public final static String DB_USER_NAME = prop.getProperty("db.username");
-    public final static String DB_PASSWORD = prop.getProperty("db.password");
-    public final static String CONNECTION_STRING = DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
-
+    public static final String DRIVER_NAME = prop.getProperty("db.driver");
+    public static final String DB_HOST = prop.getProperty("db.host");
+    public static final String DB_PORT = prop.getProperty("db.port");
+    public static final String DB_NAME = prop.getProperty("db.name");
+    public static final String DB_USER_NAME = prop.getProperty("db.username");
+    public static final String DB_PASSWORD = prop.getProperty("db.password");
+    public static final String CONNECTION_STRING = DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
 }
