75610f72b85fae76db47f73b8aa9f5d43631afe9
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-21 20:32:00 +0200

    Refactor CheckoutServlet to improve error handling and logging during login redirection and checkout processing

--- repo_git/onlinebookstore/src/main/java/servlets/CheckoutServlet.java
+++ repo_git/onlinebookstore/src/main/java/servlets/CheckoutServlet.java
@@ -13,35 +13,52 @@ import com.bittercode.constant.BookStoreConstants;
 import com.bittercode.model.UserRole;
 import com.bittercode.util.StoreUtil;
 
+import java.util.logging.Logger;
+
 public class CheckoutServlet extends HttpServlet {
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        PrintWriter pw = res.getWriter();
+
+    private static final Logger logger = Logger.getLogger(CheckoutServlet.class.getName());
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) {
         res.setContentType(BookStoreConstants.CONTENT_TYPE_TEXT_HTML);
-        if (!StoreUtil.isLoggedIn(UserRole.CUSTOMER, req.getSession())) {
-            RequestDispatcher rd = req.getRequestDispatcher("CustomerLogin.html");
-            rd.include(req, res);
-            pw.println("<table class=\"tab\"><tr><td>Please Login First to Continue!!</td></tr></table>");
+        PrintWriter pw;
+
+        try {
+            pw = res.getWriter();
+        } catch (IOException e) {
+            logger.severe("Errore durante l'ottenimento del PrintWriter: " + e.getMessage());
             return;
         }
-        try {
 
+        if (!StoreUtil.isLoggedIn(UserRole.CUSTOMER, req.getSession())) {
+            try {
+                RequestDispatcher rd = req.getRequestDispatcher("CustomerLogin.html");
+                rd.include(req, res);
+                pw.println("<table class=\"tab\"><tr><td>Please Login First to Continue!!</td></tr></table>");
+            } catch (ServletException | IOException e) {
+                logger.severe("Errore durante il redirect alla pagina di login: " + e.getMessage());
+            }
+            return;
+        }
+
+        try {
             RequestDispatcher rd = req.getRequestDispatcher("payment.html");
             rd.include(req, res);
+
             StoreUtil.setActiveTab(pw, "cart");
+
+            Object amount = req.getSession().getAttribute("amountToPay");
+
             pw.println("Total Amount<span class=\"price\" style=\"color: black\"><b>&#8377; "
-                    + req.getSession().getAttribute("amountToPay")
-                    + "</b></span>");
+                    + amount + "</b></span>");
+            pw.println("<input type=\"submit\" value=\"Pay & Place Order\" class=\"btn\">");
+            pw.println("</form></div></div></div></div>");
 
-            pw.println("<input type=\"submit\" value=\"Pay & Place Order\" class=\"btn\">"
-                    + "</form>");
-
-            pw.println("</div>\r\n"
-                    + " </div>\r\n"
-                    + " </div>\r\n"
-                    + " </div>");
+        } catch (ServletException | IOException e) {
+            logger.severe("Errore durante l'elaborazione del checkout: " + e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.severe("Errore generico nel checkout: " + e.getMessage());
         }
     }
-
 }
