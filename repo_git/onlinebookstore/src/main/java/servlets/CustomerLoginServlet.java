ffe467b662b97702c887f16138e2dbdf09006d84
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-22 11:25:24 +0200

    Refactor CustomerLoginServlet to enhance error handling and improve code readability

--- repo_git/onlinebookstore/src/main/java/servlets/CustomerLoginServlet.java
+++ repo_git/onlinebookstore/src/main/java/servlets/CustomerLoginServlet.java
@@ -5,12 +5,11 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 import com.bittercode.constant.BookStoreConstants;
 import com.bittercode.constant.db.UsersDBConstants;
+import com.bittercode.model.StoreException;
 import com.bittercode.model.User;
 import com.bittercode.model.UserRole;
 import com.bittercode.service.UserService;
@@ -20,24 +19,41 @@ public class CustomerLoginServlet extends HttpServlet {
 
     UserService authService = new UserServiceImpl();
 
+    @Override
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        PrintWriter pw = res.getWriter();
+        PrintWriter pw;
+        try {
+            pw = res.getWriter();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
         res.setContentType(BookStoreConstants.CONTENT_TYPE_TEXT_HTML);
+
         String uName = req.getParameter(UsersDBConstants.COLUMN_USERNAME);
         String pWord = req.getParameter(UsersDBConstants.COLUMN_PASSWORD);
-        User user = authService.login(UserRole.CUSTOMER, uName, pWord, req.getSession());
+
+        User user = null;
+        try {
+            user = authService.login(UserRole.CUSTOMER, uName, pWord, req.getSession());
+        } catch (StoreException e) {
+            e.printStackTrace();
+            RequestDispatcher rd = req.getRequestDispatcher("CustomerLogin.html");
+            rd.include(req, res);
+            pw.println("<table class=\"tab\"><tr><td>Internal error occurred. Please try again later.</td></tr></table>");
+            return;
+        }
 
         try {
-
             if (user != null) {
-
                 RequestDispatcher rd = req.getRequestDispatcher("CustomerHome.html");
                 rd.include(req, res);
                 pw.println("    <div id=\"topmid\"><h1>Welcome to Online <br>Book Store</h1></div>\r\n"
                         + "    <br>\r\n"
                         + "    <table class=\"tab\">\r\n"
                         + "        <tr>\r\n"
-                        + "            <td><p>Welcome "+user.getFirstName()+", Happy Learning !!</p></td>\r\n"
+                        + "            <td><p>Welcome " + user.getFirstName() + ", Happy Learning !!</p></td>\r\n"
                         + "        </tr>\r\n"
                         + "    </table>");
 
