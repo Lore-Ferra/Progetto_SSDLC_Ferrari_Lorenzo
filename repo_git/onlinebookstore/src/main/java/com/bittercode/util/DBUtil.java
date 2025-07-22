c95388376e6bbcc21c1f7470f4ed97bb298c4115
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-22 12:13:22 +0200

    Refactor DBUtil to streamline connection handling and improve test environment support

--- repo_git/onlinebookstore/src/main/java/com/bittercode/util/DBUtil.java
+++ repo_git/onlinebookstore/src/main/java/com/bittercode/util/DBUtil.java
@@ -9,31 +9,22 @@ import com.bittercode.model.StoreException;
 
 public class DBUtil {
 
-    private static Connection connection;
-
-    static {
-
-        try {
-
-            Class.forName(DatabaseConfig.DRIVER_NAME);
-            
-            connection = DriverManager.getConnection(DatabaseConfig.CONNECTION_STRING, DatabaseConfig.DB_USER_NAME,
-                    DatabaseConfig.DB_PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-
-            e.printStackTrace();
-
-        }
-
-    }// End of static block
-
     public static Connection getConnection() throws StoreException {
-
-        if (connection == null) {
+        try {
+            if (isTestEnvironment()) {
+                Class.forName("org.h2.Driver");
+                return DriverManager.getConnection("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1", "sa", "");
+            } else {
+                Class.forName(DatabaseConfig.DRIVER_NAME);
+                return DriverManager.getConnection(DatabaseConfig.CONNECTION_STRING,
+                        DatabaseConfig.DB_USER_NAME, DatabaseConfig.DB_PASSWORD);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
             throw new StoreException(ResponseCode.DATABASE_CONNECTION_FAILURE);
         }
-
-        return connection;
     }
 
+    private static boolean isTestEnvironment() {
+        return "true".equalsIgnoreCase(System.getProperty("test.env"));
+    }
 }
