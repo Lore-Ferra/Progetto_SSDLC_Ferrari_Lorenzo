cbfe3242dfbffc86a767822ee4d0b7f902307a92
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-29 18:33:10 +0200

    Refactor BooksDBConstants: change from interface to final class for improved encapsulation and prevent instantiation

--- repo_git/onlinebookstore/src/main/java/com/bittercode/constant/db/BooksDBConstants.java
+++ repo_git/onlinebookstore/src/main/java/com/bittercode/constant/db/BooksDBConstants.java
@@ -1,12 +1,16 @@
 package com.bittercode.constant.db;
 
-public interface BooksDBConstants {
-	
-    public static String TABLE_BOOK = "books";
-	
-	public static String COLUMN_NAME = "name";
-	public static String COLUMN_BARCODE = "barcode";
-	public static String COLUMN_AUTHOR = "author";
-	public static String COLUMN_PRICE = "price";
-	public static String COLUMN_QUANTITY = "quantity";
+public final class BooksDBConstants {
+
+    private BooksDBConstants() {
+        // Previene l'istanza della classe
+    }
+
+    public static final String TABLE_BOOK = "books";
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_BARCODE = "barcode";
+    public static final String COLUMN_AUTHOR = "author";
+    public static final String COLUMN_PRICE = "price";
+    public static final String COLUMN_QUANTITY = "quantity";
 }
