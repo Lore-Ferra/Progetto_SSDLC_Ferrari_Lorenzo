0df7c55d14f6c58f9ea28a2da1878b4b0b33bfc3
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-30 10:37:48 +0200

    Refactor BookServiceImpl: streamline book creation in getBooksByCommaSeparatedBookIds method

--- repo_git/onlinebookstore/src/main/java/com/bittercode/service/impl/BookServiceImpl.java
+++ repo_git/onlinebookstore/src/main/java/com/bittercode/service/impl/BookServiceImpl.java
@@ -148,6 +148,7 @@ public class BookServiceImpl implements BookService {
 
         Connection con = DBUtil.getConnection();
         String[] ids = commaSeparatedBookIds.split(",");
+
         String placeholders = String.join(",", java.util.Collections.nCopies(ids.length, "?"));
         String query = SELECT_ALL_FROM + BooksDBConstants.TABLE_BOOK + SQL_WHERE +
                 BooksDBConstants.COLUMN_BARCODE + " IN (" + placeholders + ")";
@@ -164,9 +165,7 @@ public class BookServiceImpl implements BookService {
                     String bAuthor = rs.getString(3);
                     int bPrice = rs.getInt(4);
                     int bQty = rs.getInt(5);
-
-                    Book book = new Book(bCode, bName, bAuthor, bPrice, bQty);
-                    books.add(book);
+                    books.add(new Book(bCode, bName, bAuthor, bPrice, bQty));
                 }
             }
         } catch (SQLException e) {
