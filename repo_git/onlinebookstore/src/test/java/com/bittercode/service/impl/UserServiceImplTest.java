0e7455c3aed1207eb77f8ac2712d543d07ece0a4
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-30 11:20:05 +0200

    Remove public modifier from UserServiceImplTest class

--- repo_git/onlinebookstore/src/test/java/com/bittercode/service/impl/UserServiceImplTest.java
+++ repo_git/onlinebookstore/src/test/java/com/bittercode/service/impl/UserServiceImplTest.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class UserServiceImplTest {
+class UserServiceImplTest {
 
     private UserServiceImpl userService;
     private HttpSession session;
